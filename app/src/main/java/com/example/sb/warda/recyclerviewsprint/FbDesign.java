ca8a79afe191727a9abeeca3aa5bbc9c42a51569
Warda Uzair <warda.sb1@gmail.com>
2018-09-15 15:48:20 +0500

    Commited

---
+++ app/src/main/java/com/example/sb/warda/recyclerviewsprint/FbDesign.java
@@ -0,0 +1,13 @@
+package com.example.sb.warda.recyclerviewsprint;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FbDesign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fb_design);
+    }
+}
